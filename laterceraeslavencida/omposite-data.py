c20234d6c469cda3bd3175611cf805214d4f9d6f
MichaelSancivier <sanciviermichael@gmail.com>
2023-10-12 01:33:16 +0000

    primeiro commit

---
+++ laterceraeslavencida/omposite-data.py
@@ -0,0 +1,20 @@
+numero = int(input("Digite un numero?"))
+
+if numero== 10:
+    print("acertou")
+    
+elif numero >11:
+     print("acertou de novo")
+     
+     
+banana = int(input("Cuantas bananas voce tem?"))
+
+if banana >= 5:
+    print ("tenho muitas bananas")
+    
+elif banana >= 1 :
+     print ("tenho poucas bananas")
+        
+else
+     print ("Nao tenho bananas")
+    
